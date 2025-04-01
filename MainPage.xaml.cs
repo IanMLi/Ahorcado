a73706a87b84f916f2471dd77265482bb88dacc2
IanMLi <ianmikaelli@gmail.com>
2025-03-31 20:46:02 -0600

    Se agrego el teclado

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,10 +2,35 @@
 
 public partial class MainPage : ContentPage
 {
+	//Regio para configurar las propiedades de IU Elementos de enlace
+	#region IU
+	
+	//Enlace para crear el teclado de la app
+	public List<char> Letters
+	{
+		get => letras;
+		set
+		{
+			letras = value;
+			OnPropertyChanged();
+		}
+	} 
 
+	#endregion
+	
+	//Region para configurar los campos de la app
+	#region Fields
+	
+		private List<char> letras = new List<char>();  //Inicializa los valores del teclado
+	
+	#endregion
+
+	//Constructor para invocar todos los elementos
 	public MainPage()
 	{
 		InitializeComponent();
+		Letters.AddRange("abcdefghijklmnopqrstuvwxyz"); //Valores del teclado
+		BindingContext = this; //Invocar todos los enlaces necesarios
 	}
 	
 //manejador del boton de reiniciar juego
